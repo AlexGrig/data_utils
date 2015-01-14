2d7d71f8ed1c6cf8f9648a66ea74de3783c669c5
Alexander Grigorievskiy <alex.grigorievskiy@gmail.com>
2015-01-14 16:11:26 +0200

    First commit
    Initial commit to the repository, contain the following files:
    	new file:   .gitignore
    	new file:   data_utils.py
    	new file:   numeric_utils.py
    	new file:   timer.py

---
+++ data_utils.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+"""
+In this module collection of useful data manipulation utils are provided.
+
+Author: Alexander Grigorievskiy, Aalto University, 2013.
+"""
+
+import numpy as np
+try: # if bottle neck is not imported, use numpy functions
+    import bottleneck as bn
+except ImportError:
+    bn = np 
+
+
+def normalize(D, ntype=0, means=None, stds=None):
+    """
+    Function which mormalizes the data. Rows of D - data points.
+    
+    Original varible D is not modified, new variables are allocated from scratch.
+    
+    When external means and stds are not provided, means and stds
+    from the data are calculated.    
+    
+    Input:
+        D - data
+        ntype - normalization type
+            0 - zero mean unit variance
+            1 - zero mean, [-1,1]
+            2 - zero mean, unit length (equclidean norm)
+            3 - only zero mean, do not scale the variance
+            
+        means - external means. For instance means of part of the dataset
+        stds - external stds. For instance means of part of the dataset
+    Output:
+        result - normalized vector
+        means - vector of column means
+        stds - change of scale. In case ntype=0 these are standard deviations
+    """
+    
+    if (not isinstance(D,np.ndarray)) or (len(D.shape) > 2):
+        raise AssertionError("Input D must be derivative of numpy.ndarray and have less than 3 dimensions.")
+    
+    (D,initial_shape) = ensure_column(D)
+            
+    n_rows = D.shape[0]    
+    
+    if means is None:
+        means = bn.nanmean(D, axis= 0)    
+    
+    tmp = D - np.tile( means, (n_rows,1) ) # temporary result. Data with 
+       # substracted mean                                                                         
+  
+    if stds is None:
+        if (ntype == 0):    
+            stds = bn.nanstd(tmp,axis=0, ddof=1 ) # one degree of freadom as matlab default
+            
+        elif (ntype == 1):
+            stds = bn.nanmax(np.abs(tmp), axis=0)
+            
+        elif (ntype == 2):    
+            stds = np.sqrt( bn.nansum( np.power(tmp,2) , axis = 0) )                
+        
+        elif (ntype == 3):    
+            stds = np.ones( (D.shape[1],) )
+            
+        else:
+            raise ValueError("Normalization type %s is unknown" % ntype)
+    
+    # result = np.dot(  tmp ,  np.diagflat( 1./stds  ) )
+    result = np.divide( tmp, stds ) 
+    
+    result = rev_ensure_column(result,initial_shape)
+    D = rev_ensure_column(D,initial_shape)    
+    
+    return (result,means,stds)
+    
+    
+def denormalize(D, means, stds=None):
+    """
+    Denormalizes the data using means and stds.
+    
+    Original varible D is not modified, new variables are allocated from scratch.
+    
+    Output:
+        result - denormalized vector
+    """    
+    
+    (D,initial_shape) = ensure_column(D)    
+    
+    n_rows =  D.shape[0]    
+    
+    if stds != None:
+        result = np.multiply(  D, stds  ) + np.tile( means, (n_rows,1) )
+    else:
+        result = D + np.tile( means, (n_rows,1) )
+    
+    result = rev_ensure_column(result,initial_shape)
+    D = rev_ensure_column(D,initial_shape)    
+    
+    return result    
+
+
+def ensure_column(v):
+    """
+    Function affects only one dimensioanl arrays ( including (1,n) and (n,1) dimensional)
+    It then represent the output as a (n,1) vector strictly. It also returns
+    initial shape by using which original dimensions of the vector can be reconstructed.
+        
+    For more than one dimensional vector function do nothing.
+        
+    Inputs:
+        v - array
+        
+    Output:
+        col - the same array with (n,1) dimensions - column
+        params - params by which original shape can be restored
+    """
+    
+    initial_shape = v.shape
+    if len(initial_shape) == 1: # one dimensional array
+        v.shape = (initial_shape[0],1)
+    else:
+        if (len(initial_shape) == 2)  and (initial_shape[0] == 1): # row vector        
+            v.shape = (initial_shape[1],1)     
+
+    return v,initial_shape
+
+
+def rev_ensure_column(v,initial_shape):
+    """
+        This function is reverse with respect to ensure_coulmn
+        It restores the original dimensions of the vector
+        
+    """
+    if initial_shape: # check that the tuple is nonempty
+        v.shape = initial_shape    
+    
+    return v
+    
+    
+def vector_len( vector ):
+    """
+    Function determines dimension of a vector.
+    E. g. if vector has a shape (1,len) or (len,1) or (len,)
+    it returns len.
+    
+    Input:
+        vector - vector of type ndarray or matrix. If both dimensions are larger 
+                 than 1 error is returned.
+                 
+    Output:
+        len - length of a vector.
+    """
+        
+    if not isinstance(vector, np.ndarray ):
+        return len(vector)
+    else:
+        shape = vector.shape # shape is a tuple
+        
+        sl = len(shape)
+        if sl == 0:
+            return 0
+        elif sl == 1:
+            return shape[0]
+        else: 
+            non_one_dims = [ s for s in shape if s > 1 ]
+            non_one_dims_len = len(non_one_dims)
+            if non_one_dims_len > 1:
+                raise ValueError("Function vector_len: Not a vector provided, shape : %s", shape)
+            elif non_one_dims_len == 0:
+                return 1
+            else:
+                return non_one_dims[0]
---
+++ numeric_utils.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Alexander Grigorevskiy. Aalto University, 2013.
+
+
+"""
+
+import numpy as np
+import data_utils as du
+
+class Inverse_Update(object):
+    """    
+    Instance of this class provide the fuctions which updates the inverse
+    of a Gram matrix. Even more general than inverse because diagonal matrix
+    can be added. 
+    
+    Function returns update to this inverse
+    ( X.T * X  + Diag )^(-1)    
+    
+    when new columns to matrix X is added.    
+    
+    Details are given in:
+    Mark JL Orr. Introduction to Radial Basis Function Networks.
+    Centre for Cognitive Science, University of Edinburgh, Technical
+    Report, April 1996. 
+    """
+    
+    def  __init__(self):   
+        """
+        Constructor
+        """
+        
+        self.prev_inv = None # inverse from previous iteration
+        self.prev_size = None #  size of the previous inverse (symmetric)
+    
+    def new_column(self, new_col, new_lambda=0, X=None, prev_inverse=None):     
+        """
+        Returns the updated inverse matrix. Original parameters are not modified. 
+        
+        Input: 
+            new_col - new colum of matrix X
+            new_lambda - (optional, float) new diagonal value
+            X - old matrix X (without new column)
+            prev_inverse - if we want to start not from the first iteration.
+                            We provide already computed inverse matrix
+        Output:
+            New inverse matrix
+        """
+        
+        if not isinstance(new_col, np.ndarray):
+            raise AssertionError("Class Inverse_Update, function new_column: new_col is not ndarray derivative, instead %s", type(new_col) )
+            
+        new_col = np.matrix( new_col.reshape(du.vector_len(new_col),1  ) ) # copy of new_col and make it column matrix
+            
+        if new_col.shape[0] == 1: 
+            new_col = new_col.T # make sure this is column vector
+            
+        if prev_inverse:
+            self.prev_inv = np.matrix( prev_inverse ) # copy of new_col and make it a matrix
+            self.prev_size = self.prev_inv.shape[0] # prev_inv is a square matrix
+
+                
+        if self.prev_inv == None: # first iteration
+            
+            self.prev_inv = 1./ ( new_col.T * new_col + new_lambda)
+            self.prev_size = 1
+            
+            return self.prev_inv
+            
+        else: # not the first iteration
+            n = self.prev_size
+            
+            
+            # Part 1 of the formula: add one more zero row and zero column
+            P1 = np.hstack(  ( self.prev_inv, np.matrix( (0,)* n ,dtype='d').T ) )
+            P1 = np.vstack( (P1, np.matrix( (0,)* (n+1),dtype='d')) )
+            
+            T = self.prev_inv * X.T # temporary expression
+            
+            delta = new_lambda + new_col.T * ( new_col - X * ( T * new_col) )
+            delta = delta[0,0] # to make constant
+            
+            v = np.vstack(( T * new_col, -1)) # vector for part 2        
+            
+            # Part 2
+            P2 = np.outer(v,v)
+            
+            self.prev_inv = P1 + 1/delta * P2
+            self.prev_size = self.prev_inv.shape[0]
+            
+            return self.prev_inv            
+
+def sqr_euclid(v):
+    """
+    Square of euclidean norm for a vector.
+    
+    Introduced because sometimes squared norm is enough and square root 
+    computation is an extrta cost.
+    
+    Input:
+        v - input vector
+        
+    Output:
+        Squared euclidean norm.
+    """
+    
+    return ((v.conj()*v).real).sum()
+        
